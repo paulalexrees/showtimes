818ced775489f393e70f4781b3d7dc11696d12c1
Paul <paulalexrees@gmail.com>
2016-11-23 16:44:38 +0000

    fixes port 3000 issues

--- app/app.js
+++ app/app.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  res.send('Hello World!');
+});
 
 module.exports = app;
 if (!module.parent) {
-  http.createServer(app).listen(process.env.PORT, function(){
-    console.log("Server listening on port " + app.get('port'));
+  http.createServer(app).listen(3000, function(){
+    console.log("Server listening on port 3000");
   });
 }
